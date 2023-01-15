36d95cc734ce7a62f502e6fbd2c72b71730912b4
Mohmammad Naimul Alam <naimul2707@gmail.com>
2023-01-15 13:03:54 +0100

    chat page setup initial

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
-// import Register from './Register'
-// import Login from './Login'
+import Sidebar from '../components/Sidebar'
+import Chat from '../components/Chat/Chat'
+
 
 const Home = () => {
   return (
-    <div>
-      {/* <Register></Register> */}
-      {/* <Login></Login> */}
+    <div className="container mx-auto p-5">
+      <div class="flex flex-row rounded-tr-xl">
+        <div class="basis-1/3 bg-gray-500 rounded-tl-2xl rounded-bl-2xl">
+          <Sidebar></Sidebar>
+        </div>
+        <div class="basis-2/3 rounded-2xl">
+          <div className="flex items-center justify-center h-screen  bg-slate-800 rounded-tr-2xl rounded-br-2xl">
+            <Chat></Chat>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
