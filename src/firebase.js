31c36f79ab480dc13ac68aa80e71c67eaafd047c
Mohmammad Naimul Alam <naimul2707@gmail.com>
2023-02-10 22:48:38 +0100

    chat app another format project initialization

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC5G8ejEsGft29nMRbDrgLvhBp6X_5iUHo",
+  authDomain: "hello-chat-naim.firebaseapp.com",
+  projectId: "hello-chat-naim",
+  storageBucket: "hello-chat-naim.appspot.com",
+  messagingSenderId: "946033554004",
+  appId: "1:946033554004:web:4216717e0e84b926be33c1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
+export const storage = getStorage();
+export const db = getFirestore();
